ac18f48fab46e2df76aa3bd8b420c1aa002161aa
LEONARDO TARQUINI <112579939+LeonardoTarqu1ni@users.noreply.github.com>
2023-03-09 17:48:38 -0300

    Add files via upload

---
+++ maior de 3.py
@@ -0,0 +1,15 @@
+#Faça um Programa que leia três números e mostre o maior deles.
+
+print(">>>Olá!<<<")
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro número: "))
+n3 = float(input("Digite mais um número: "))
+
+if n1 > n2 and n1 > n3:
+    print("{} é o maior de todos!".format(n1))
+
+elif n3 > n1 and n3 > n2:
+    print("{} é o maior de todos!".format(n3))
+
+elif n2 > n1 and n2 > n3:
+    print("{} é o maior de todos!".format(n2))
